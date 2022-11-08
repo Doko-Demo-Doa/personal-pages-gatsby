1829bda9f4ba0b5120a58b08efcf22a3c694d649
Quan Pham <doraemonfanclub@gmail.com>
2022-11-08 10:17:47 +0700

    Init

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import { Box, Container } from '@mantine/core';
+import React from 'react';
+
+export const Home = () => (
+  <Container>
+    <Box>sadsada</Box>
+  </Container>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/Layout/Layout';
+import { Home } from '../components/Home/Home';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
