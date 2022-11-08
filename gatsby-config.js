a74d08ae9bd7498ce8d1d4a0682a5ec9580b56de
Quan Pham <doraemonfanclub@gmail.com>
2022-11-08 10:34:53 +0700

    Add new page sample

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
   plugins: ['gatsby-plugin-mantine'],
+  siteMetadata: {
+    title: 'Quan',
+    description: 'This is my personal site template.',
+  },
 };
---
+++ src/pages/test-query.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const TestQuery = () => <div>Hello!</div>;
+
+export const query = graphql`
+  # Queries will go here
+`;
+
+export default TestQuery;
