7fd748a869e3c6feefd9810ccf5c4b1aa0b1a094
Quan Pham <doraemonfanclub@gmail.com>
2022-11-09 16:13:36 +0700

    Update to Gatsby 5 with config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  plugins: ['gatsby-plugin-mantine'],
+  // plugins: ['gatsby-plugin-mantine'],
   siteMetadata: {
     title: 'Quan',
     description: 'This is my personal site template.',
   },
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
